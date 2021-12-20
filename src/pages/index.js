3e486e1980ab4754163aabc5e35123ac798e25cc
David Shams <david.shams.innova@gmail.com>
2021-12-19 20:53:18 -0600

    Style React components with CSS Modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function IndexPage() {
   return (
     <div>
-      Gatsby
+      <Layout pageTitle={"Page Title"}>Gatsby</Layout>
     </div>
   )
 }
