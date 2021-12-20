aaa24e1d62742407770a12fd75afc77a50d2b591
David Shams <david.shams.innova@gmail.com>
2021-12-20 12:54:58 -0600

    Contentful data

---
+++ src/components/InsightComponent.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+
+const InsightComponent = () => (
+  <StaticQuery
+    query={graphql`
+      {
+        allContentfulInsights {
+          edges {
+            node {
+              article {
+                article
+                id
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => <pre>{JSON.stringify(data, null, 4)}</pre>}
+  ></StaticQuery>
+)
+
+export default InsightComponent
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Header from "../components/header"
+import InsightComponent from "../components/InsightComponent"
+import InsightComponent2 from "./what-if"
+
+
 
 export default function IndexPage() {
   return (
     <div>
       <Layout pageTitle={"Page Title"}>Gatsby</Layout>
+      <Header>
+        <InsightComponent />
+      </Header>
+      <InsightComponent2 />
     </div>
   )
 }
